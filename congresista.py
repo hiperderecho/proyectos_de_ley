78da50427bb7bcfa5c0490c49c5496f9c601758b
aniversarioperu <aniversarioperu1@gmail.com>
2013-11-09 12:35:56 +0200

    fixed hiperlinks to congresista name

--- congresista.py
+++ congresista.py
@@ -9,6 +9,37 @@ import sys
 import string
 import re
 
+def convert_name_to_filename(name):
+    # takes a congresista name and returns its html filename
+    name = name.replace(",", "").lower()
+    name = name.encode("ascii", "ignore")
+    name = name.split(" ")
+    
+    if len(name) > 2:
+        i = 0
+        path = ""
+        while( i < 3 ):
+            path += name[i]
+            if i < 2:
+                path += "_"
+            i = i + 1
+        path = os.path.join("congresista", path)
+        filename = os.path.join(path, "index.html")
+        return filename
+
+def hiperlink_congre(congresistas):
+    # tries to make a hiperlink for each congresista name to its own webpage
+    for name in congresistas.split("; "):
+        filename = convert_name_to_filename(name)
+        if filename:
+            if os.path.isfile(filename):
+                link = "<a href='"
+                link += os.path.join("../..", filename)
+                link += "' title='ver todos sus proyectos'>"
+                link += name + "</a>"
+                congresistas = congresistas.replace(name, link)
+    return congresistas
+
 def myjson(data):
     return json.dumps(data, sort_keys=True, indent=4, separators=(',', ':'))
 
@@ -17,7 +48,7 @@ def prettify(item):
     out += "\n<div>\n"
     out += "<p><b>" + item['numero_proyecto'] + "</b>\n"
     out += "<h4>" + item['titulo'] +  "</h4>\n"
-    out += "<p>" + item['congresistas'] + "</p>\n"
+    out += "<p>" + hiperlink_congre(item['congresistas']) + "</p>\n"
 
     if 'pdf_url' in item:
         out += "<a class='btn btn-lg btn-primary'"
--- scrape.py
+++ scrape.py
@@ -58,13 +58,46 @@ def generate_html():
     f.write(html)
     f.close()
 
+def hiperlink_congre(congresistas):
+    # tries to make a hiperlink for each congresista name to its own webpage
+    for name in congresistas.split("; "):
+        filename = convert_name_to_filename(name)
+        if filename:
+            if os.path.isfile(filename):
+                link = "<a href='"
+                link += os.path.dirname(__file__)
+                link += filename
+                link += "' title='ver todos sus proyectos'>"
+                link += name + "</a>"
+                congresistas = congresistas.replace(name, link)
+    return congresistas
+
+def convert_name_to_filename(name):
+    # takes a congresista name and returns its html filename
+    name = name.replace(",", "").lower()
+    name = name.encode("ascii", "ignore")
+    name = name.split(" ")
+    
+    if len(name) > 2:
+        i = 0
+        path = ""
+        while( i < 3 ):
+            path += name[i]
+            if i < 2:
+                path += "_"
+            i = i + 1
+        path = os.path.join("congresista", path)
+        filename = os.path.join(path, "index.html")
+        return filename
+
+
 def prettify(data):
     out = ""
     for item in data:
         out += "\n<div>\n"
         out += "<p><b>" + item['numero_proyecto'] + "</b>\n"
         out += "<h4>" + item['titulo'] +  "</h4>\n"
-        out += "<p>" + item['congresistas'] + "</p>\n"
+        out += "<p>" + hiperlink_congre(item['congresistas']) + "</p>\n"
 
         if 'pdf_url' in item:
             out += "<a class='btn btn-lg btn-primary'"

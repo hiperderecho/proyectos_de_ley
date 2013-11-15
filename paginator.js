e41f9a09250aeb530a8a36352bfc3d8fe5c3fcc4
aniversarioperu <aniversarioperu1@gmail.com>
2013-11-15 07:05:35 +0200

    fix paginator in congresista pages

--- paginator.js
+++ paginator.js
@@ -1,3 +1,8 @@
+var loc = window.location.pathname.split("/");
+var reg = /congresista/;
+if(reg.test($(this).val())) {
+    alert(loc[1]);
+}
 $.getJSON( "html.json", function(data){
     var myarray = [];
     $.each( data, function( key, val) {

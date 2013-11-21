5b64225f20fbdda65cf5c2bba412d69e3e5c1c0b
aniversarioperu <aniversarioperu1@gmail.com>
2013-11-21 16:09:49 +0200

    Still use only javascript to show data for congresista

--- paginator.js
+++ paginator.js
@@ -1,7 +1,34 @@
 var loc = window.location.pathname;
 if (loc.indexOf("congresista") >= 0) {
-    console.log(loc);
+    //console.log(loc);
     // load html.doc
+    $.getJSON( 'html.json', function(data) {
+        var myarray = [];
+        $.each( data, function(key,val) {
+            myarray.push(val);
+        });
+        myarray.sort().reverse();
+        //console.log(myarray);
+        $("#contenido").html(myarray.slice(0,20));
+
+        // number of pages (20 items per page)
+        var n_pages = parseInt(myarray.length / 20) + 1;
+        //
+        // init bootpag
+        $('#page-selection').bootpag({
+            total: n_pages,
+            page: 1,
+            maxVisible: 20
+        }).on("page", function(event, /* page number here */ num){
+            var start = 20;
+            var end = start*num;
+            //console.log(myarray.slice(end - 20, end));
+            $("#contenido").fadeOut("fast", function() {
+            $("#contenido").fadeIn("slow", function(){}).html(myarray.slice(end - 20, end)); // some ajax content loading...
+            $('html, body').animate({ scrollTop: 0 }, 0);
+        });
+      });
+   });
 }
 else {
 

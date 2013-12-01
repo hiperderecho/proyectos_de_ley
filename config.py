98f705ecad0865ad7734f34e08ad497380f0496f
aniversarioperu <aniversarioperu1@gmail.com>
2013-12-01 14:10:25 +0200

    need some info in config file

--- config.py
+++ config.py
@@ -15,9 +15,16 @@ Ejemplo:
 #current_folder = "/home/script_user/scripts/"
 #legislatura = "2011"
 
+### Otros datos importantes
+#title = u'Proyectos de ley emitidos por el Congreso de la República del Perú'
+#description = ''
+
 
 ### 
 base_folder = "/data/projects/aniversario_peru_github/hiperderecho_proyectos_de_ley/"
 base_url = "localhost/hiperderecho/"
 current_folder = "/data/projects/aniversario_peru_github/hiperderecho_proyectos_de_ley/"
 legislatura = "2011"
+
+title = u'Proyectos de ley emitidos por el Congreso de la República del Perú'
+description = ''
